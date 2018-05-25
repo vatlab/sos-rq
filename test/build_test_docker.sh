eb0345a52365e042ba057036aa7792c748a7fe44
Bo Peng <ben.bog@gmail.com>
2018-05-25 00:01:40 -0500

    Try to fix rq docker

--- test/build_test_docker.sh
+++ test/build_test_docker.sh
@@ -40,11 +40,11 @@ ADD authorized_keys /root/.ssh/authorized_keys
 
 # install sos on the remote host
 RUN  pip install spyder jedi notebook nbconvert nbformat pyyaml psutil tqdm
-RUN  pip install fasteners pygments ipython ptpython networkx pydotplus sos sos-notebook sos-bash
+RUN  pip install fasteners pygments ipython ptpython networkx pydotplus sos sos-notebook
 
 ARG  SHA=LATEST
-RUN  SHA=$SHA git clone http://github.com/vatlab/sos-pbs sos-pbs
-RUN  cd sos-pbs && python setup.py install
+RUN  SHA=$SHA git clone http://github.com/vatlab/sos-rq sos-rq
+RUN  cd sos-rq && pip install . -U
 
 RUN  echo "export TS_SLOTS=10" >> /root/.bash_profile
 

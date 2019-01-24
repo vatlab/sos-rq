99c3a463a83d35b04c93a39e2fd9eded859c5d7b
Bo Peng <ben.bog@gmail.com>
2019-01-24 09:16:06 -0600

    Release sos rq 0.18.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name = "sos-rq",
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Console',
-        'License :: OSI Approved :: BSD',
+        'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS :: MacOS X',
@@ -38,7 +38,7 @@ setup(name = "sos-rq",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires=[
-          'sos>=0.17.4',
+          'sos>=0.18.0',
           'rq',
       ],
     entry_points= '''
